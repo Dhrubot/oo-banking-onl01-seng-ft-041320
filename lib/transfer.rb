7118520b6c0a4bb454b3d8816cd814b04f0639f1
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-30 11:13:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
 class Transfer
   # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver, :transfer_amount
+  
+  def initialize(sender, receiver, status = "pending", transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @transfer_amount = transfer_amount
+  end
+  
 end
