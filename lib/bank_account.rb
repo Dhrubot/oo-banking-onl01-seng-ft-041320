7e6d28c3d0067d86a3852af576eced6525c7925b
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-30 11:07:55 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,10 +2,15 @@ class BankAccount
   
   attr_accessor :balance, :status
   attr_reader :name
+  
   def initialize(name, balance = 1000, status = "open")
     @name = name
     @balance = balance
     @status = status
+  end
+  
+  def deposit
+    
   end
   
 end
